a4b299d10aadb581f42bbbca9307a865cb806282
Victor Varela <vitaotapioqueiro@gmail.com>
2021-08-26 14:39:14 -0300

    falta o reduce

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@
 function callbackNewForEach(element, indice, array) {
     return undefined
 }
-Array.prototype.newForEach = function(callback){
-    for (let i = 0;  i < this.length; i++){
+Array.prototype.newForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
         callback(this[i], i, this);
     }
 }
@@ -15,19 +15,19 @@ Array.prototype.newForEach = function(callback){
 
 // 02 = FILL
 
-Array.prototype.newFill = function(change, start, end) {
-    for (let i = 0; i < this.length; i++){
-        if (start !== undefined){
-            if (end !== undefined){
-                if (i >= start && i < end){
+Array.prototype.newFill = function (change, start, end) {
+    for (let i = 0; i < this.length; i++) {
+        if (start !== undefined) {
+            if (end !== undefined) {
+                if (i >= start && i < end) {
                     array[i] = change
                 }
             } else {
-                if (i >= start){
+                if (i >= start) {
                     array[i] = change
                 }
             }
-        } else{
+        } else {
             array[i] = change
         }
     }
@@ -39,12 +39,12 @@ Array.prototype.newFill = function(change, start, end) {
 // 03 - MAP
 
 function callbackNewMap(element, index, array) {
-    return element%2
+    return element
 }
 
-Array.prototype.newMap = function(callback){
+Array.prototype.newMap = function (callback) {
     let newArray = []
-    for (let i = 0; i < this.length; i++){
+    for (let i = 0; i < this.length; i++) {
         newArray.push(callback(this[i], i, this))
     }
     return newArray
@@ -58,11 +58,9 @@ function callbackNewSome(element, index, array) {
     return element
 }
 
-Array.prototype.newSome = function(callback) {
-    let output = undefined;
-    for (let i = 0; i < this.length; i++){
-        output = callback(callbackNewSome(this[i], i, this))
-        if (output === true){
+Array.prototype.newSome = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (output = callback(this[i], i, this)) {
             return true
         }
     }
@@ -77,15 +75,12 @@ function callbackNewFind(element, index, array) {
     return element
 }
 
-Array.prototype.newFind = function(callback) {
-    let output = ''
-    for (let i = 0; i < this.length; i++){
-        output = callback(this[i], i, this)
-        if (output === true){
-            return this[i]
-        }
+Array.prototype.newFind = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+        return this[i]
+
     }
-    //return undefined
 }
 
 
@@ -95,11 +90,12 @@ function callbackNewFindIndex(element, index, array) {
     return element
 }
 
-Array.prototype.newFindIndex = function(callback) {
+Array.prototype.newFindIndex = function (callback) {
     let output = ''
-    for (let i = 0; i < this.length; i++){
+    for (let i = 0; i < this.length; i++) {
         output = callback(this[i], i, this)
-        if (output === true){
+        // off
+        if (output === true) {
             return i
         }
     }
@@ -113,11 +109,9 @@ function callbackNewEvery(element, index, array) {
     return element
 }
 
-Array.prototype.newEvery = function (callback){
-    let output = ''
-    for (let i = 0; i < this.length; i++){
-        output = callback(this[i], i, this)
-        if (output === false){
+Array.prototype.newEvery = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (!callback(this[i], i, this)) {
             return false
         }
     }
@@ -132,10 +126,10 @@ function callbackNewFilter(element, index, array) {
     return element
 }
 
-Array.prototype.newFilter = function (callback){
+Array.prototype.newFilter = function (callback) {
     let output = []
-    for (let i = 0; i < this.length; i++){
-        if (callback(this[i], i, this)){
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
             output.push(this[i])
         }
     }
@@ -145,3 +139,87 @@ Array.prototype.newFilter = function (callback){
 
 
 // 09 - CONCAT
+
+Array.prototype.newConcat = function (array) {
+    let output = [...this]
+    for (let i = 0; i < array.length; i++){
+        output.push(array[i])
+    }
+    return output
+}
+
+
+
+// 10 - INCLUDES
+
+Array.prototype.newIncludes = function (searchElement, fromIndex) {
+    let i = this.length + fromIndex;
+
+    if (i === undefined){
+        i = 0
+    }
+    
+    for (i; i < this.length; i++){
+        if (this[i] === searchElement){
+            return true
+        }
+    }
+    return false
+}
+
+
+
+// 11 - INDEXOF
+
+Array.prototype.newIndexOf = function (searchElement, fromIndex){
+    if (fromIndex >= this.length){
+        return -1
+    }
+
+    if (fromIndex < 0){
+        for (let i = this.length; i >= this.length + fromIndex; i--){
+            if (this[i] === searchElement){
+                return true
+            }
+        }
+        return false
+    }
+
+    let i = 0 + fromIndex;
+
+    if (fromIndex === undefined){
+        i = 0
+    }
+
+    for (i; i < this.length; i++){
+        if (this[i] === searchElement){
+            return 1
+        }
+    }
+    return -1
+}
+
+
+// 12 - JOIN
+
+Array.prototype.newJoin = function (separator) {
+    let output = ''
+    for (let i = 0; i < this.length; i++){
+        output += this[i]
+        if (i < this.length - 1){
+            if (separator === undefined){
+                output += ','
+            } else {
+                output += separator
+            }
+        }
+    }
+    return output
+}
+
+
+// 13 - REDUCE
+
+Array.prototype.newReduce = function () {
+    
+}
